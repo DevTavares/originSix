2c501b1b383e6e395df9db75a141d2fcaf8e259e
Rodrigo Tavares <rtsantana@outlook.com.br>
2021-08-03 20:30:44 -0300

    Desktop: Project completed

--- main.js
+++ main.js
@@ -18,9 +18,10 @@ for (const link of links) {
 }
 
 /* Change header of page when page move down */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
 
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
@@ -37,7 +38,13 @@ const swiper = new Swiper ('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel:true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 });
 
 /* ScrollReveal: Show elements when scroll in page */
@@ -60,8 +67,9 @@ scrollReveal.reveal(
   )
 
 /* Buttom back to top*/
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
 
   if(window.scrollY >=560) {
     backToTopButton.classList.add('show')
@@ -69,8 +77,34 @@ function backToTop() {
     backToTopButton.classList.remove('show')
   }
 }
+/* Menu ativo conforme seção visivel na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
-window.addEventListener('scroll', function() {
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll*/
+window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
-  backToTop() 
+  backToTop()
+  activateMenuAtCurrentSection()
 })
